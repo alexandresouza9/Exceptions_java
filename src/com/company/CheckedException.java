0bee0d5b16b77a8f17a78e8b8d33818dedb18f4f
alexandresouza9 <alexandresouza9@gmail.com>
2022-03-02 13:28:36 -0300

    fisrt

---
+++ src/com/company/CheckedException.java
@@ -0,0 +1,16 @@
+package com.company;
+import javax.swing.*;
+import java.io.*;
+
+
+public class CheckedException {
+    public static void main(String[] args){
+        String nomeDoArquivo = "romance-blake-crouch.txt";
+        imprimirArquivoNoConsole(nomeDoArquivo);
+
+        System.out.println("Apesar da Exception ou não, o programa continua....");
+    }
+    public static void imprimirArquivoNoConsole(String nomeDoArquivo){
+
+    }
+}
